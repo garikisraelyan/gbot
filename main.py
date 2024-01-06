bb7b8a580d32b3664bce0795d01395f0a0ecc5e6
garikisraelyan <garikisrayelyan@gmail.com>
2024-01-06 05:43:19 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes
+)
+
+from tools.yt_video_downloader import (
+    message_processer, InvalidRequestException
+)
+
+load_dotenv()
+
+TOKEN: str = os.getenv("TOKEN")
+BOT_USERNAME: str = os.getenv("BOT_USERNAME")
+
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("""Hi! I can download videos for you. 
+For more information send `/help`.""")
+    
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+"""Here are the list of commands you can use:
+/help
+/test
+
+Or just send a YouTube video link and I'll send that video back to you!""")
+
+
+async def test_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "This was supposed to test if the bot is working :)"
+    )
+
+
+# Handlers
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text: str = update.message.text
+    print(f"User {update.message.chat.id}: {text}")
+
+    try:
+        file_path = message_processer(text)
+        if file_path:
+            with open(file_path, 'rb') as file:
+                await update.message.reply_video(
+                    video=file, supports_streaming=True
+                )
+    except InvalidRequestException as e:
+        print(e)
+
+if __name__ == "__main__":
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("test", test_command))
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    app.run_polling(poll_interval=3)
+
+
+# TODO: Replace print statements with logger
---
+++ tools/yt_video_downloader.py
@@ -0,0 +1,40 @@
+from pytube import YouTube
+
+SAVE_PATH = ""
+
+
+class InvalidRequestException(Exception):
+    def __init__(
+            self, 
+            message: str = "Invalid request. Please provide a YouTube video link."
+        ):
+        self.message = message
+        super().__init__(self.message)
+
+
+def download(link: str, resolution: str = None):
+    yt = YouTube(link).streams
+    if resolution:
+        yt = yt.filter(res=resolution).first()
+        print(yt)
+    else:
+        yt = yt.get_highest_resolution()
+        print(yt)
+    filename = f"{yt.title}_{resolution}.mp4".replace(" ", "_")
+    file = yt.download(output_path=SAVE_PATH, filename=filename)
+    return file
+
+def message_processer(message: str):
+    if ("https://www.youtube.com/watch?" or
+        "https://youtube.com/watch?") in message:
+        splitted_message = message.split(" ")
+        for word in splitted_message:
+            if ("https://www.youtube.com/watch?" or
+            "https://youtube.com/watch?") in word:
+                yt_link = word
+                break
+        file = download(yt_link)
+        return file
+    # TODO: Exception
+    # else:
+    #     raise InvalidRequestException
